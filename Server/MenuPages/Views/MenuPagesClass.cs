1f77724a93632f9c7a5bc3fbd3af91fa4d08e878
jay <jay@contensive.com>
2018-04-26 17:51:40 -0400

    let admin go through page blocks automatically -- then need to edit content

--- Server/MenuPages/Views/MenuPagesClass.cs
+++ Server/MenuPages/Views/MenuPagesClass.cs
@@ -5,55 +5,44 @@ using System.Text;
 using System.Linq;
 using Contensive.BaseClasses;
 
-namespace Contensive.Addons.BootstrapNav.Views
-{
+namespace Contensive.Addons.BootstrapNav.Views {
     /// <summary>
     /// menuing based on pages directly attached to the menu (menuPageRules)
     /// </summary>
-    public class MenuPagesClass : Contensive.BaseClasses.AddonBaseClass
-    {
+    public class MenuPagesClass : Contensive.BaseClasses.AddonBaseClass {
         //
         // -- instance properties
         CPBaseClass cp;
         //
         // ====================================================================================================
         //
-        public override object Execute(Contensive.BaseClasses.CPBaseClass cp)
-        {
+        public override object Execute(Contensive.BaseClasses.CPBaseClass cp) {
             this.cp = cp;
             string result = "";
-            try
-            {
+            try {
                 //
                 // -- get Menu
                 int menuId = cp.Doc.GetInteger(constants.rnSelectMenuId);
                 Models.Entity.menuModel menu = null;
-                if (menuId != 0)
-                {
+                if (menuId != 0) {
                     menu = Models.Entity.menuModel.create(cp, menuId);
                 }
-                if (menu == null)
-                {
+                if (menu == null) {
                     // -- No menu selected, try instance menu
                     string instanceId = cp.Doc.GetText("instanceId");
-                    if (string.IsNullOrEmpty(instanceId))
-                    {
+                    if (string.IsNullOrEmpty(instanceId)) {
                         // -- no instanceId, find or create default menu
                         menu = Models.Entity.menuModel.createByName(cp, "Default");
-                        if (menu == null)
-                        {
+                        if (menu == null) {
                             // -- no Default Menu, create it
                             menu = Models.Entity.menuModel.add(cp);
                             menu.name = "Default";
                             menu.save(cp);
                         }
-                    }
-                    else
-                    {
+                    } else {
                         // -- find or create instance menu
                         menu = Models.Entity.menuModel.create(cp, instanceId);
-                        if (menu == null)
-                        {
+                        if (menu == null) {
                             // -- no Default Menu, create it
                             menu = Models.Entity.menuModel.add(cp);
                             menu.ccguid = instanceId;
@@ -63,26 +52,20 @@ namespace Contensive.Addons.BootstrapNav.Views
                         }
                     }
                 }
-                if (menu == null)
-                {
+                if (menu == null) {
                     result = "<!-- Selected Menu not found -->";
-                }
-                else
-                {
+                } else {
                     //
                     // -- create toplists
                     StringBuilder topItemList = new StringBuilder();
                     string sql = "(AllowInMenus=1)and(id in (select pageId from ccMenuPageRules where menuID=" + menu.id + "))";
                     List<Models.Entity.pageContentModel> rootPageList = Models.Entity.pageContentModel.createList(cp, sql);
-                    foreach (Models.Entity.pageContentModel rootPage in rootPageList)
-                    {
-                        bool blockRootPage = rootPage.BlockContent;
-                        if (blockRootPage & cp.User.IsAuthenticated)
-                        {
+                    foreach (Models.Entity.pageContentModel rootPage in rootPageList) {
+                        bool blockRootPage = rootPage.BlockContent & !cp.User.IsAdmin;
+                        if (blockRootPage & cp.User.IsAuthenticated) {
                             blockRootPage = !allowedPageIdList.Contains(rootPage.id);
                         }
-                        if (!blockRootPage)
-                        {
+                        if (!blockRootPage) {
                             string classTopItem = menu.classTopItem;
                             if (!string.IsNullOrEmpty(rootPage.menuClass)) { classTopItem += " " + rootPage.menuClass; }
                             if (rootPage == rootPageList.First()) { classTopItem += " " + menu.classItemFirst; }
@@ -101,15 +84,12 @@ namespace Contensive.Addons.BootstrapNav.Views
                             if (childPageList.Count == 0) { classTierItem += " " + menu.classItemLast; }
                             itemHtmlId = string.Format("menu{0}Page{1}", menu.id.ToString(), rootPage.id.ToString());
                             tierItemList.Append(cp.Html.li(getAnchor(cp, rootPage, menu.classTierAnchor), "", classTopItem, itemHtmlId));
-                            foreach (Models.Entity.pageContentModel childPage in childPageList)
-                            {
+                            foreach (Models.Entity.pageContentModel childPage in childPageList) {
                                 bool blockPage = childPage.BlockContent;
-                                if (blockPage & cp.User.IsAuthenticated)
-                                {
+                                if (blockPage & cp.User.IsAuthenticated) {
                                     blockPage = !allowedPageIdList.Contains(childPage.id);
                                 }
-                                if (!blockPage)
-                                {
+                                if (!blockPage) {
                                     //if (!menu.addRootToTier)
                                     //{
                                     //    if (childPage == childPageList.First()) { classTierItem += " " + menu.classItemFirst; }
@@ -126,15 +106,12 @@ namespace Contensive.Addons.BootstrapNav.Views
                         }
                     }
                     result = cp.Html.ul(topItemList.ToString(), "menu" + menu.id.ToString() + "List", menu.classTopList);
-                    if (!string.IsNullOrEmpty(menu.classTopWrapper) )
-                    {
+                    if (!string.IsNullOrEmpty(menu.classTopWrapper)) {
                         result = cp.Html.div(result, "", menu.classTopWrapper);
                     }
-                    
+
                 }
-            }
-            catch (Exception ex)
-            {
+            } catch (Exception ex) {
                 cp.Site.ErrorReport(ex);
                 result = "error response";
             }
@@ -142,30 +119,23 @@ namespace Contensive.Addons.BootstrapNav.Views
         }
         //
         // -- create a listItem from a page
-        private string getAnchor(CPBaseClass cp, Models.Entity.pageContentModel page, string htmlClass)
-        {
-            try
-            {
+        private string getAnchor(CPBaseClass cp, Models.Entity.pageContentModel page, string htmlClass) {
+            try {
                 string topItemCaption = page.MenuHeadline;
                 if (string.IsNullOrEmpty(topItemCaption)) topItemCaption = page.name;
                 string pageLink = cp.Content.GetPageLink(page.id);
                 //string pageList = cp.Content.GetLinkAliasByPageID(page.id, "", "");
                 return string.Format("<a class=\"{2}\" title=\"{1}\" href=\"{0}\">{1}</a>", pageLink, topItemCaption, htmlClass);
-            }
-            catch (Exception ex)
-            {
+            } catch (Exception ex) {
                 cp.Site.ErrorReport(ex);
             }
             return "";
         }
         //
         // -- list of pages explicitly allowed by this users group membership
-        private List<int> allowedPageIdList
-        {
-            get
-            {
-                if (_allowedPageIdList == null)
-                {
+        private List<int> allowedPageIdList {
+            get {
+                if (_allowedPageIdList == null) {
                     _allowedPageIdList = Models.Entity.pageContentModel.getAllowedPageIdList(cp);
                 }
                 return _allowedPageIdList;
@@ -174,12 +144,9 @@ namespace Contensive.Addons.BootstrapNav.Views
         private List<int> _allowedPageIdList = null;
         //
         // -- list of sections explicitly allowed by this users group membership
-        private List<int> allowedSectionIdList
-        {
-            get
-            {
-                if (_allowedSectionIdList == null)
-                {
+        private List<int> allowedSectionIdList {
+            get {
+                if (_allowedSectionIdList == null) {
                     _allowedSectionIdList = Models.Entity.siteSectionsModel.getAllowedSectionIdList(cp);
                 }
                 return _allowedSectionIdList;
--- Server/MenuPages/Views/NavbarBasicDropdown.cs
+++ Server/MenuPages/Views/NavbarBasicDropdown.cs
@@ -5,55 +5,44 @@ using System.Text;
 using System.Linq;
 using Contensive.BaseClasses;
 
-namespace Contensive.Addons.BootstrapNav
-{
+namespace Contensive.Addons.BootstrapNav {
     /// <summary>
     /// menuing based on pages directly attached to the menu (menuPageRules)
     /// </summary>
-    public class NavbarBasicDropdownClass : Contensive.BaseClasses.AddonBaseClass
-    {
+    public class NavbarBasicDropdownClass : Contensive.BaseClasses.AddonBaseClass {
         //
         // -- instance properties
         CPBaseClass cp;
         //
         // ====================================================================================================
         //
-        public override object Execute(Contensive.BaseClasses.CPBaseClass cp)
-        {
+        public override object Execute(Contensive.BaseClasses.CPBaseClass cp) {
             this.cp = cp;
             string result = "";
-            try
-            {
+            try {
                 //
                 // -- get Menu
                 int menuId = cp.Doc.GetInteger(constants.rnSelectMenuId);
                 Models.Entity.menuModel menu = null;
-                if (menuId != 0)
-                {
+                if (menuId != 0) {
                     menu = Models.Entity.menuModel.create(cp, menuId);
                 }
-                if (menu == null)
-                {
+                if (menu == null) {
                     // -- No menu selected, try instance menu
                     string instanceId = cp.Doc.GetText("instanceId");
-                    if (string.IsNullOrEmpty(instanceId))
-                    {
+                    if (string.IsNullOrEmpty(instanceId)) {
                         // -- no instanceId, find or create default menu
                         menu = Models.Entity.menuModel.createByName(cp, "Default");
-                        if (menu == null)
-                        {
+                        if (menu == null) {
                             // -- no Default Menu, create it
                             menu = Models.Entity.menuModel.add(cp);
                             menu.name = "Default";
                             menu.save(cp);
                         }
-                    }
-                    else
-                    {
+                    } else {
                         // -- find or create instance menu
                         menu = Models.Entity.menuModel.create(cp, instanceId);
-                        if (menu == null)
-                        {
+                        if (menu == null) {
                             // -- no Default Menu, create it
                             menu = Models.Entity.menuModel.add(cp);
                             menu.ccguid = instanceId;
@@ -77,26 +66,20 @@ namespace Contensive.Addons.BootstrapNav
                         }
                     }
                 }
-                if (menu == null)
-                {
+                if (menu == null) {
                     result = "<!-- Selected Menu not found -->";
-                }
-                else
-                {
+                } else {
                     //
                     // -- create toplists
                     StringBuilder topItemList = new StringBuilder();
                     string sql = "(AllowInMenus=1)and(id in (select pageId from ccMenuPageRules where menuID=" + menu.id + "))";
                     List<Models.Entity.pageContentModel> rootPageList = Models.Entity.pageContentModel.createList(cp, sql);
-                    foreach (Models.Entity.pageContentModel rootPage in rootPageList)
-                    {
-                        bool blockRootPage = rootPage.BlockContent;
-                        if (blockRootPage & cp.User.IsAuthenticated)
-                        {
+                    foreach (Models.Entity.pageContentModel rootPage in rootPageList) {
+                        bool blockRootPage = rootPage.BlockContent & !cp.User.IsAdmin;
+                        if (blockRootPage & cp.User.IsAuthenticated) {
                             blockRootPage = !allowedPageIdList.Contains(rootPage.id);
                         }
-                        if (!blockRootPage)
-                        {
+                        if (!blockRootPage) {
                             string classTopItem = menu.classTopItem;
                             if (!string.IsNullOrEmpty(rootPage.menuClass)) { classTopItem += " " + rootPage.menuClass; }
                             if (rootPage == rootPageList.First()) { classTopItem += " " + menu.classItemFirst; }
@@ -110,13 +93,11 @@ namespace Contensive.Addons.BootstrapNav
                             StringBuilder tierItemList = new StringBuilder();
                             sql = "(ParentID=" + rootPage.id + ")";
                             List<Models.Entity.pageContentModel> childPageList = Models.Entity.pageContentModel.createList(cp, sql);
-                            if (childPageList.Count == 0)
-                            {
+                            if (childPageList.Count == 0) {
                                 //
                                 // -- no dropdown
-                            }
-                            else
-                            {
+                                topItemList.Append(cp.Html.li(getAnchor(cp, rootPage, classTopAnchor, ""), "", classTopItem));
+                            } else {
                                 //
                                 // -- dropdown nav
                                 classTopItem += " " + menu.classTopParentItem;
@@ -128,35 +109,29 @@ namespace Contensive.Addons.BootstrapNav
                                 string classTierAnchor = menu.classTierAnchor;
                                 classTierItem += " " + menu.classItemFirst;
                                 tierItemList.Append(cp.Html.li(getAnchor(cp, rootPage, classTierAnchor, ""), "", classTopItem));
-                                foreach (Models.Entity.pageContentModel childPage in childPageList)
-                                {
+                                foreach (Models.Entity.pageContentModel childPage in childPageList) {
                                     bool blockPage = childPage.BlockContent;
-                                    if (blockPage & cp.User.IsAuthenticated)
-                                    {
+                                    if (blockPage & cp.User.IsAuthenticated) {
                                         blockPage = !allowedPageIdList.Contains(childPage.id);
                                     }
-                                    if (!blockPage)
-                                    {
+                                    if (!blockPage) {
                                         if (childPage == childPageList.Last()) { classTierItem += " " + menu.classItemLast; }
                                         if (!string.IsNullOrEmpty(childPage.menuClass)) { classTierItem += " " + childPage.menuClass; }
                                         tierItemList.Append(cp.Html.li(getAnchor(cp, childPage, menu.classTierAnchor, ""), "", classTierItem));
                                     }
                                 }
+                                tierList = cp.Html.ul(tierItemList.ToString(), "", menu.classTierList);
+                                topItemList.Append(cp.Html.li(getAnchor(cp, rootPage, classTopAnchor, menu.dataToggleTopParentAnchor) + tierList, "", classTopItem));
                             }
-                            tierList = cp.Html.ul(tierItemList.ToString(), "", menu.classTierList);
-                            topItemList.Append(cp.Html.li(getAnchor(cp, rootPage, classTopAnchor, menu.dataToggleTopParentAnchor) + tierList, "", classTopItem));
                         }
                     }
-                    result = cp.Html.ul(topItemList.ToString(),"", menu.classTopList);
-                    if (!string.IsNullOrEmpty(menu.classTopWrapper) )
-                    {
+                    result = cp.Html.ul(topItemList.ToString(), "", menu.classTopList);
+                    if (!string.IsNullOrEmpty(menu.classTopWrapper)) {
                         result = cp.Html.div(result, "", menu.classTopWrapper);
                     }
-                    
+
                 }
-            }
-            catch (Exception ex)
-            {
+            } catch (Exception ex) {
                 cp.Site.ErrorReport(ex);
                 result = "error response";
             }
@@ -164,10 +139,8 @@ namespace Contensive.Addons.BootstrapNav
         }
         //
         // -- create a listItem from a page
-        private string getAnchor(CPBaseClass cp, Models.Entity.pageContentModel page, string htmlClass, string dataToggleValue)
-        {
-            try
-            {
+        private string getAnchor(CPBaseClass cp, Models.Entity.pageContentModel page, string htmlClass, string dataToggleValue) {
+            try {
                 string topItemCaption = page.MenuHeadline;
                 if (string.IsNullOrEmpty(topItemCaption)) topItemCaption = page.name;
                 string pageLink = cp.Content.GetPageLink(page.id);
@@ -175,21 +148,16 @@ namespace Contensive.Addons.BootstrapNav
                 if (string.IsNullOrEmpty(dataToggleValue)) { dataToggleAttr = string.Empty; } else { dataToggleAttr = " data-toggle=\"" + dataToggleValue + "\""; }
                 //string pageList = cp.Content.GetLinkAliasByPageID(page.id, "", "");
                 return string.Format("<a class=\"{2}\" title=\"{1}\" href=\"{0}\"{3}>{1}</a>", pageLink, topItemCaption, htmlClass, dataToggleAttr);
-            }
-            catch (Exception ex)
-            {
+            } catch (Exception ex) {
                 cp.Site.ErrorReport(ex);
             }
             return "";
         }
         //
         // -- list of pages explicitly allowed by this users group membership
-        private List<int> allowedPageIdList
-        {
-            get
-            {
-                if (_allowedPageIdList == null)
-                {
+        private List<int> allowedPageIdList {
+            get {
+                if (_allowedPageIdList == null) {
                     _allowedPageIdList = Models.Entity.pageContentModel.getAllowedPageIdList(cp);
                 }
                 return _allowedPageIdList;
@@ -198,12 +166,9 @@ namespace Contensive.Addons.BootstrapNav
         private List<int> _allowedPageIdList = null;
         //
         // -- list of sections explicitly allowed by this users group membership
-        private List<int> allowedSectionIdList
-        {
-            get
-            {
-                if (_allowedSectionIdList == null)
-                {
+        private List<int> allowedSectionIdList {
+            get {
+                if (_allowedSectionIdList == null) {
                     _allowedSectionIdList = Models.Entity.siteSectionsModel.getAllowedSectionIdList(cp);
                 }
                 return _allowedSectionIdList;
