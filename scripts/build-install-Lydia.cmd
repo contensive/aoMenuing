b18f6314e083e83dcd7445185529852f81442100
jkidwell <jay@contensive.com>
2024-05-24 14:42:24 -0400

    updates

---
+++ scripts/build-install-Lydia.cmd
@@ -0,0 +1,12 @@
+
+rem all paths are relative to the git scripts folder
+
+set appName=lydiakidwell
+
+call build.cmd
+
+rem upload to contensive application
+c:
+cd %collectionPath%
+cc -a %appName% --installFile "%collectionName%.zip"
+cd ..\..\scripts
